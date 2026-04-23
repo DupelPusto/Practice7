a3e69981a183b71d7fba4423784cc7d185c0491c
Боріс Євгеній Вадимович <borisjenya2@gmail.com>
2026-04-23 10:48:20 +0300

    Now MyVector implements Comparable

--- src/MyVector.java
+++ src/MyVector.java
@@ -1,12 +1,12 @@
 import exceptions.*;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class MyVector<T> {
+public class MyVector<T> implements Comparable<MyVector<T>> {
     private T[] values;
     private int size = 0;
     private int defaultCapacity = 10;
-
 
     public MyVector() {
         Object[] value = new Object[defaultCapacity];
@@ -29,7 +29,6 @@ public class MyVector<T> {
     }
 
     public void addOnIndex(int index, T value){
-
         if (value == null) throw new NullElementException("Елемент не може бути null");
         if (index < 0 || index > size) throw new InvalidIndexException("Невірний індекс для додавання елемента");
 
@@ -43,8 +42,7 @@ public class MyVector<T> {
     }
 
     public void clear(){
-
-        if (size == 0) throw new ClearException("ектор вже порожній");
+        if (size == 0) throw new ClearException("Вектор вже порожній");
         for (int i = 0; i < size; i++) {
             values[i] = null;
         }
@@ -53,9 +51,7 @@ public class MyVector<T> {
     }
 
     public void remove(int index) throws EmptyMyVectorException {
-
         if (size == 0) throw new EmptyMyVectorException("Неможливо видалити елемент. Вектор порожній");
-
         if (index < 0 || index >= size) throw new InvalidIndexException("Невірний індекс для видалення");
 
         for (int i = index; i < size - 1; i++) {
@@ -95,10 +91,31 @@ public class MyVector<T> {
     public String toString() {
         String str = "Values: ";
         for (int i = 0; i < size; i++) {
-            if (i == values.length) str += values[i];
-            str += values[i] + "; ";
+            str += values[i] + (i < size - 1 ? "; " : "");
         }
         return str;
     }
 
+    //Compare by size
+    @Override
+    public int compareTo(MyVector<T> o) {
+        return Integer.compare(this.size, o.size);
+    }
+
+
+    public void sort() {
+        if (size > 1) {
+            Object[] mas = Arrays.copyOf(values, size);
+            Arrays.sort(mas);
+            for (int i = 0; i < size; i++) {
+                values[i] = (T) mas [i];
+            }
+        }
+    }
+
+    public void sort(Comparator<? super T> comparator) {
+        if (size > 1) {
+            Arrays.sort(values, 0,size, comparator);
+        }
+    }
 }
